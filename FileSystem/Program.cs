92b0776d0a22c5faed856134a4becd042548117b
Your Name <you@example.com>
2021-10-26 14:08:11 +0300

    Week9

---
+++ FileSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userDirectory = "wishList";
+
+            string newDirectoryFullPath = @$"{rootDirectory}\{userDirectory}";
+            Console.WriteLine(newDirectoryFullPath);
+
+            Directory.CreateDirectory(newDirectoryFullPath);
+            
+        }
+    }
+}
